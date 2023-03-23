b3121159d938f02d6a8d96ee74da533264fc36b9
Samuel Ume <cume357@gmail.com>
2023-03-23 23:08:32 +0100

    Finished first intance of Taskify

---
+++ src/model.ts
@@ -0,0 +1,17 @@
+export interface Todo {
+  id: number;
+  todo: string;
+  isDone: boolean;
+}
+
+// import React from 'react'
+
+// const model = () => {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default model

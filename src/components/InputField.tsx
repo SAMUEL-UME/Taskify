5cad209262e757d0989cd9fc75f595696396f18b
Samuel Ume <cume357@gmail.com>
2023-03-24 16:46:04 +0100

    Finished Takify build

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,32 +1,36 @@
 import React, { useRef } from "react";
 import "./styles.css";
 
-interface Props {
+interface props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
   handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputFeild: React.FC<Props> = ({ todo, setTodo, handleAdd }: Props) => {
+const InputField: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
   const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-      <form className="input" onSubmit={(e) => {
-          handleAdd(e)
-          inputRef.current?.blur()
-      }}>
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
       <input
-        ref={inputRef}
-        type="input"
+        type="text"
+        placeholder="Enter a Task"
         value={todo}
+        ref={inputRef}
         onChange={(e) => setTodo(e.target.value)}
-        placeholder="Enter a task"
         className="input__box"
       />
-      <button className="input__submit" type="submit">
-        Go
+      <button type="submit" className="input_submit">
+        GO
       </button>
     </form>
   );
 };
 
-export default InputFeild;
+export default InputField;
